842f2734bde3fec50806173f183e56581aeb176c
unknown <dejiolojede@gmail.com>
2021-11-09 03:49:09 +0200

    Added APi routes and UserController to manage user actions

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Lesson;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Events\LessonWatched;
+use App\Events\CommentWritten;
+use Illuminate\Http\JsonResponse;
+use App\Events\AchievementUnlocked;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
+class UserController extends Controller
+{
+    /**
+     * enroll a user in a lesson
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function enroll(Request $request): JsonResponse
+    {
+        $success = false;
+        $userId = $request->input('user_id');
+        $lessonId = $request->input('lesson_id');
+        $user = User::find($userId);
+        if ($user && $lessonId) {
+            if ($user->lessons()->syncWithoutDetaching($lessonId)) {
+                $success = true;
+            }
+        }
+        return response()->json(['success' => $success]);
+    }
+
+    /**
+     * User creates a comment
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function comment(Request $request): JsonResponse
+    {
+        $success = false;
+        $userId = $request->input('user_id');
+        $user = User::find($userId);
+        if ($user) {
+            $comment = new Comment;
+            $comment->user_id = $userId;
+            $comment->body = $request->input('body');
+            if ($comment->save()) {
+                $success = true;
+                return response()->json([
+                    'success' => $success,
+                    'comment' => $comment
+                ]);
+            }
+        }
+        return response()->json(['success' => $success]);
+    }
+
+    /**
+     * User completes a lesson
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function lessonWatched(Request $request): JsonResponse
+    {
+        $success = false;
+        $userId = $request->input('user_id');
+        $lessonId = $request->input('lesson_id');
+        $user = User::find($userId);
+        $lesson = Lesson::find($lessonId);
+        if ($user && $lesson) {
+            if ($user->lessons()->updateExistingPivot($lesson->id, ['watched' => true])) {
+                $success = true;
+                return response()->json(['success' => $success]);
+            }
+        }
+        return response()->json(['success' => $success]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AchievementsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+ * Added this routes so i can test via Postman since there is no frontend
+ */
+Route::post('/user/enroll/lesson', [UserController::class, 'enroll']);
+
+Route::post('/user/comment', [UserController::class, 'comment']);
+
+Route::post('/user/lesson/watched', [UserController::class, 'lessonWatched']);
+
+Route::get('/user/{user}/achievements', [AchievementsController::class, 'index']);
