72f241e78473c10f73887da36c594887e4930c97
unknown <dejiolojede@gmail.com>
2021-11-09 04:59:08 +0200

    renamed example test and added test for achievement statistics

--- tests/Feature/AchievementStatisticsTest.php
+++ tests/Feature/AchievementStatisticsTest.php
@@ -5,22 +5,22 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends TestCase
+class AchievementStatisticsTest extends TestCase
 {
-    use DatabaseTransactions;
+    use DatabaseTransactions, DatabaseMigrations;
 
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function test_achievement_statistics()
     {
         $user = User::factory()->create();
-        
+
         $response = $this->get("/users/{$user->id}/achievements");
 
+        $response->assertOk();
         $response->assertStatus(200);
     }
 }
