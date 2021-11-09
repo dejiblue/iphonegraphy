b3442a1b66ace0244c298e5ae91ed9b7451082ee
unknown <dejiolojede@gmail.com>
2021-11-09 03:40:17 +0200

    updated the databse seeder to create a test user and attach lessons to the user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Lesson;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,5 +19,18 @@ class DatabaseSeeder extends Seeder
         $lessons = Lesson::factory()
             ->count(20)
             ->create();
+
+        // Only create the first user with email user1@test.com
+        if (!User::whereEmail('user1@test.com')->first()) {
+            $user = User::create([
+                'name' => 'Test User',
+                'email' => 'user1@test.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('User1@2021'), // password
+                'remember_token' => Str::random(10),
+            ]);
+
+            $user->lessons()->sync(array(1, 3, 5, 8, 15, 19, 20));
+        }
     }
 }
